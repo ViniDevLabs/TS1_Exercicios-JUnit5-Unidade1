ade07b0ec7ee4a0ab46da4bfce0403ebaa8773fd
vinithecsar <viniciuscesarbrito@gmail.com>
2025-04-29 11:43:00 -0300

    Questão 4 - Average

---
+++ src/main/java/com/testing/software/questao4/Average.java
@@ -0,0 +1,13 @@
+package com.testing.software.questao4;
+
+// Escreva testes parametrizados para o exemplo abaixo.
+
+public class Average {
+  private Average() {
+    // Non default constructor
+  }
+
+  public static double semester(int num1, int num2, int num3) {
+    return (num1 + num2 + num3) / 3.;
+  }
+}
---
+++ src/test/java/questao4/AverageTest.java
@@ -0,0 +1,30 @@
+package questao4;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.testing.software.questao4.Average;
+
+class AverageTest {
+
+  @ParameterizedTest
+  @MethodSource("valoresValidos")
+  void semestreTest(int num1, int num2, int num3, double expected) {
+    assertEquals(expected, Average.semester(num1, num2, num3), 0.01);
+  }
+
+  static Stream<Arguments> valoresValidos() {
+    return Stream.of(
+        Arguments.of(90, 85, 95, 90.0),
+        Arguments.of(100, 100, 100, 100.0),
+        Arguments.of(70, 80, 90, 80.0),
+        Arguments.of(60, 75, 85, 73.33),
+        Arguments.of(95, 88, 92, 91.66));
+  }
+
+}
